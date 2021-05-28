a28472c3a6145bd5d969b3ceb1ad6149ea1f7138
Simon Burbidge <simon@bonobolabs.com>
2021-05-28 08:48:27 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .watchOS(.v6),
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         .library(name: "PageView", targets: ["PageView"]),
